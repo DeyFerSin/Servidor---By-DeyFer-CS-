247bbf6b15253798b400589376ddc3afc0ba99d8
DeyFer <jrm_1970629@hotmail.com>
2024-05-07 23:16:12 -0600

    Agregar archivos de proyecto.

---
+++ Servidor - By DeyFer (CS)/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    'Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StartServer = New MaterialSkin.Controls.MaterialButton()
+        Me.BufferServer = New System.Windows.Forms.TextBox()
+        Me.StopServer = New MaterialSkin.Controls.MaterialButton()
+        Me.ClearLog = New MaterialSkin.Controls.MaterialButton()
+        Me.SuspendLayout()
+        '
+        'StartServer
+        '
+        Me.StartServer.AutoSize = False
+        Me.StartServer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.StartServer.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.[Default]
+        Me.StartServer.Depth = 0
+        Me.StartServer.HighEmphasis = True
+        Me.StartServer.Icon = Nothing
+        Me.StartServer.Location = New System.Drawing.Point(4, 415)
+        Me.StartServer.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.StartServer.MouseState = MaterialSkin.MouseState.HOVER
+        Me.StartServer.Name = "StartServer"
+        Me.StartServer.NoAccentTextColor = System.Drawing.Color.Empty
+        Me.StartServer.Size = New System.Drawing.Size(181, 36)
+        Me.StartServer.TabIndex = 1
+        Me.StartServer.Text = "Start Server"
+        Me.StartServer.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.StartServer.UseAccentColor = False
+        Me.StartServer.UseVisualStyleBackColor = True
+        '
+        'BufferServer
+        '
+        Me.BufferServer.BackColor = System.Drawing.Color.Black
+        Me.BufferServer.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.BufferServer.ForeColor = System.Drawing.Color.Lime
+        Me.BufferServer.Location = New System.Drawing.Point(4, 69)
+        Me.BufferServer.Multiline = True
+        Me.BufferServer.Name = "BufferServer"
+        Me.BufferServer.Size = New System.Drawing.Size(582, 337)
+        Me.BufferServer.TabIndex = 2
+        '
+        'StopServer
+        '
+        Me.StopServer.AutoSize = False
+        Me.StopServer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.StopServer.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.[Default]
+        Me.StopServer.Depth = 0
+        Me.StopServer.HighEmphasis = True
+        Me.StopServer.Icon = Nothing
+        Me.StopServer.Location = New System.Drawing.Point(206, 415)
+        Me.StopServer.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.StopServer.MouseState = MaterialSkin.MouseState.HOVER
+        Me.StopServer.Name = "StopServer"
+        Me.StopServer.NoAccentTextColor = System.Drawing.Color.Empty
+        Me.StopServer.Size = New System.Drawing.Size(181, 36)
+        Me.StopServer.TabIndex = 3
+        Me.StopServer.Text = "Stop Server"
+        Me.StopServer.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.StopServer.UseAccentColor = False
+        Me.StopServer.UseVisualStyleBackColor = True
+        '
+        'ClearLog
+        '
+        Me.ClearLog.AutoSize = False
+        Me.ClearLog.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClearLog.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.[Default]
+        Me.ClearLog.Depth = 0
+        Me.ClearLog.HighEmphasis = True
+        Me.ClearLog.Icon = Nothing
+        Me.ClearLog.Location = New System.Drawing.Point(405, 415)
+        Me.ClearLog.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.ClearLog.MouseState = MaterialSkin.MouseState.HOVER
+        Me.ClearLog.Name = "ClearLog"
+        Me.ClearLog.NoAccentTextColor = System.Drawing.Color.Empty
+        Me.ClearLog.Size = New System.Drawing.Size(181, 36)
+        Me.ClearLog.TabIndex = 4
+        Me.ClearLog.Text = "Limpiar log"
+        Me.ClearLog.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.ClearLog.UseAccentColor = False
+        Me.ClearLog.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(592, 460)
+        Me.Controls.Add(Me.ClearLog)
+        Me.Controls.Add(Me.StopServer)
+        Me.Controls.Add(Me.BufferServer)
+        Me.Controls.Add(Me.StartServer)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Servidor - By DeyFer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StartServer As MaterialSkin.Controls.MaterialButton
+    Friend WithEvents BufferServer As TextBox
+    Friend WithEvents StopServer As MaterialSkin.Controls.MaterialButton
+    Friend WithEvents ClearLog As MaterialSkin.Controls.MaterialButton
+End Class
---
+++ Servidor - By DeyFer (CS)/Form1.vb
@@ -0,0 +1,167 @@
+﻿Imports MaterialSkin
+Imports System.Text
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading
+Imports System.ComponentModel
+
+Public Class Form1
+#Region "Variables"
+    Private server As TcpListener
+    Private isServerRunning As Boolean = False
+    Dim Dinero As Integer
+#End Region
+#Region "Inicio de la aplicacion"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SkinManager As MaterialSkinManager = MaterialSkinManager.Instance
+        SkinManager.AddFormToManage(Me)
+        SkinManager.Theme = MaterialSkinManager.Themes.DARK
+        SkinManager.ColorScheme = New ColorScheme(Primary.LightBlue900, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE)
+        'Dimenciones Form
+        '592, 460
+        Me.Width = 592
+        Me.Height = 460
+        BufferServer.ForeColor = Color.Lime
+        BufferServer.BackColor = Color.Black
+    End Sub
+#End Region
+    'Cuando la app se cierra
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Me.Close()
+    End Sub
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Me.Close()
+    End Sub
+    Private Sub StartServer_()
+        Try
+            server = New TcpListener(IPAddress.Any, 3497)
+            server.Start()
+            isServerRunning = True
+            Dim serverThread As Thread = New Thread(AddressOf ListenForClients_)
+            serverThread.Start()
+            Log_("Servidor iniciado en " & IPAddress.Any.ToString() & " puerto 3497.")
+        Catch ex As Exception
+            Log_("Error al iniciar el servidor: " & ex.Message)
+        End Try
+    End Sub
+    Private Sub StopServer_()
+        If isServerRunning Then
+            server.Stop()
+            isServerRunning = False
+            Log_("Servidor detenido.")
+        End If
+    End Sub
+    Private Sub ClearLog_()
+        BufferServer.Clear()
+    End Sub
+    Private Sub Log_(message As String, Optional direction As String = "<-")
+        If BufferServer.InvokeRequired Then
+            BufferServer.Invoke(Sub() BufferServer.AppendText(direction & " " & message & Environment.NewLine))
+        Else
+            BufferServer.AppendText(direction & " " & message & Environment.NewLine)
+        End If
+    End Sub
+    Private Sub ListenForClients_()
+        Try
+            While isServerRunning
+                Dim client As TcpClient = server.AcceptTcpClient()
+                Dim clientEndPoint As Net.IPEndPoint = CType(client.Client.RemoteEndPoint, Net.IPEndPoint)
+                Log_("Cliente conectado desde " & clientEndPoint.Address.ToString() & ":" & clientEndPoint.Port.ToString())
+
+                Dim clientThread As Thread = New Thread(Sub() HandleClientRequests(client))
+                clientThread.Start()
+            End While
+        Catch ex As Exception
+            Log_("Error al aceptar cliente: " & ex.Message)
+        End Try
+    End Sub
+    Private Function WaitForResponse(stream As NetworkStream) As String
+        Log_("WaitForResponse")
+        Dim responseData As New StringBuilder()
+        Dim buffer(1024) As Byte
+        Dim bytesRead As Integer
+
+        While stream.DataAvailable
+            bytesRead = stream.Read(buffer, 0, buffer.Length)
+            responseData.Append(Encoding.ASCII.GetString(buffer, 0, bytesRead))
+        End While
+
+        Log_(responseData.ToString())
+        Return responseData.ToString()
+    End Function
+    Private Sub HandleClientRequests(client As TcpClient)
+        Try
+            Dim Dinero As Integer = 500
+
+            Log_("Nueva conexion", "<-")
+            Log_("HandleClientRequests:" & client.ToString(), "<-")
+            Dim stream As NetworkStream = client.GetStream()
+            Dim bufferSize As Integer = 1024
+            Dim data(bufferSize) As Byte
+
+            While True
+                Dim bytesRead As Integer = stream.Read(data, 0, bufferSize)
+
+                If bytesRead <= 0 Then
+                    Exit While
+                End If
+
+                Dim requestData As String = Encoding.ASCII.GetString(data, 0, bytesRead).Trim()
+                Log_("Mensaje recibido del cliente: " & requestData, "<-")
+                Select Case requestData
+                    Case "GET_INITIAL_MONEY"
+                        Dim responseBytes As Byte() = Encoding.ASCII.GetBytes(Dinero.ToString())
+                        stream.Write(responseBytes, 0, responseBytes.Length)
+                        stream.Flush()
+                        Log_("Enviando cantidad inicial de dinero al cliente: " & Dinero.ToString(), "->")
+                    Case "OPEN_CHEST"
+                        Dim random As New Random()
+                        Dim coins As Integer = random.Next(1, 101)
+                        Dinero += coins
+                        Dim response As String = "COINS_RECEIVED:" & coins.ToString()
+                        Dim responseBytes As Byte() = Encoding.ASCII.GetBytes(response)
+                        stream.Write(responseBytes, 0, responseBytes.Length)
+                        stream.Flush()
+                        Log_("Enviando monedas recibidas al cliente: " & coins.ToString(), "->")
+                    Case "BUY_CHEST"
+                        If Dinero >= 250 Then
+                            Dinero -= 250
+                            Dim response As String = "COINS_RECEIVED:-250"
+                            Dim responseBytes As Byte() = Encoding.ASCII.GetBytes(response)
+                            stream.Write(responseBytes, 0, responseBytes.Length)
+                            stream.Flush()
+                            Log_("Enviando monedas restantes al cliente: " & Dinero.ToString(), "->")
+                        Else
+                            Dim response As String = "INSUFFICIENT_FUNDS"
+                            Dim responseBytes As Byte() = Encoding.ASCII.GetBytes(response)
+                            stream.Write(responseBytes, 0, responseBytes.Length)
+                            stream.Flush()
+                            Log_("Enviando mensaje de fondos insuficientes al cliente.", "->")
+                        End If
+                    Case "CHECK_COINS"
+                        Dim response As String = "CURRENT_COINS:" & Dinero.ToString()
+                        Dim responseBytes As Byte() = Encoding.ASCII.GetBytes(response)
+                        stream.Write(responseBytes, 0, responseBytes.Length)
+                        stream.Flush()
+                        Log_("Enviando cantidad actual de monedas al cliente: " & Dinero.ToString(), "->")
+                End Select
+            End While
+        Catch ex As Exception
+            Log_("Error al manejar solicitudes del cliente: " & ex.Message)
+        Finally
+            Log_("La conexión con el cliente se mantendrá abierta.", "->")
+        End Try
+    End Sub
+    'Botones
+    Private Sub StartServer_Click(sender As Object, e As EventArgs) Handles StartServer.Click
+        StartServer_()
+    End Sub
+
+    Private Sub StopServer_Click(sender As Object, e As EventArgs) Handles StopServer.Click
+        StopServer_()
+    End Sub
+
+    Private Sub ClearLog_Click(sender As Object, e As EventArgs) Handles ClearLog.Click
+        ClearLog_()
+    End Sub
+End Class
